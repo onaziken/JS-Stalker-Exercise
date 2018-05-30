fdefff385fd104faa6192e031bbad681bbb44d1b
Patrick Azien <onaziken@gmail.com>
2018-05-30 22:21:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Yup, project file is connected\!");
+
+var firstName = prompt("What is your firstname?");
+var lastName = prompt("What's your lastname?");
+var fullName = firstName + " " + lastName
+var age = prompt("How old are you?");
+
+console.log("Hi " + fullName + ", you are " + age + " years old!");
